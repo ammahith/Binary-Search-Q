9a359bbb7502bffe56d92376bd4876da40e93208
Abdullah Mahith <106341998+ammahith@users.noreply.github.com>
2023-11-13 18:52:32 -0500

    Sorted Permutation type medium

---
+++ Sorted Permutations.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+void permute(string s, string answer)
+{
+	if (s.length() == 0) {
+		cout << answer << " ";
+		return;
+	}
+	for (int i = 0; i < s.length(); i++) {
+		char ch = s[i];
+		string left_substr = s.substr(0, i);
+		string right_substr = s.substr(i + 1);
+		string rest = left_substr + right_substr;
+		permute(rest, answer + ch);
+	}
+}
+int main()
+{
+	string s = "ABC";
+	string answer = "";
+
+	cout << "\nAll possible strings are : ";
+	permute(s, answer);
+	return 0;
+}
